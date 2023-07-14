c20b19a13f96ad82c2768b93e00c5859914b84ac
Shivam_Shivanshu <shivamshivanshu15@gmail.com>
2023-07-14 09:09:29 +0000

    Initial Draft

---
+++ tensorfox/rolling_ohlc/src/lib.rs
@@ -0,0 +1,41 @@
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+pub struct Ohlc {
+    pub open: f64,
+    pub high: f64,
+    pub low: f64,
+    pub close: f64,
+}
+
+pub struct RollingOhlc {
+    window: u64,
+    prices: VecDeque<(u64, f64)>,
+}
+
+impl RollingOhlc {
+    pub fn new(window: u64) -> Self {
+        RollingOhlc {
+            window: window,
+            prices: VecDeque::new(),
+        }
+    }
+    pub fn update(&mut self, timestamp: u64, price: f64) -> Option<Ohlc> {
+        self.prices.push_back((timestamp, price));
+        while let Some((ts, _)) = self.prices.front() {
+            if timestamp - ts > self.window {
+                self.prices.pop_front();
+            } else {
+                break;
+            }
+        }
+        if let Some((_, open)) = self.prices.front() {
+            let close = price;
+            let high = self.prices.iter().map(|(_, p)| *p).fold(f64::MIN, f64::max);
+            let low = self.prices.iter().map(|(_, p)| *p).fold(f64::MAX, f64::min);
+            Some(Ohlc{open : *open, high, low, close})
+        } else {
+            None
+        }
+    }
+}
--- tensorfox/src/main.rs
+++ tensorfox/src/main.rs
@@ -1,3 +1,77 @@
+use rolling_ohlc::{RollingOhlc};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
+#[derive(Deserialize)]
+struct PriceData {
+    e: String,
+    u: u64,
+    s: String,
+    b: String,
+    B: String,
+    a: String,
+    A: String,
+    T: u64,
+    E: u64,
+}
+struct PriceDataPased {
+    symbol : String, 
+    timestamp : u64,
+    price : f64,
+}
+
+#[derive(Serialize)]
+pub struct OhlcData {
+    pub symbol: String, 
+    pub timestamp: u64,
+    pub open: f64,
+    pub high: f64,
+    pub low: f64,
+    pub close: f64,
+}
+
+
 fn main() {
-    println!("Hello World");
+    let window: u64 = 300;
+    let file = File::open("../data/dataset-a.txt").expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let mut prices : Vec<PriceDataPased> = Vec::new();
+    for line in reader.lines() {
+        if let Ok(json_str) = line {
+            let curr : PriceData = serde_json::from_str(&json_str).expect("Failed to deserialize JSON");
+            let curr_ohcl = PriceDataPased {
+                symbol: curr.s,
+                timestamp: curr.T,
+                price: curr.b.parse::<f64>().unwrap(),
+            };
+            prices.push(curr_ohcl);
+        } else {
+            println!("Failed to read line");
+            break;
+        }
+    }
+    let mut ohlcs = std::collections::HashMap::new();
+    let mut ohlc_values: Vec<OhlcData> = Vec::new();
+    for price in prices {
+        let ohlc = ohlcs
+            .entry(price.symbol.clone())
+            .or_insert_with(|| RollingOhlc::new(window));
+        if let Some(ohlc) = ohlc.update(price.timestamp, price.price) {
+            ohlc_values.push(OhlcData {
+                symbol: price.symbol,
+                timestamp: price.timestamp,
+                open: ohlc.open,
+                high: ohlc.high,
+                low: ohlc.low,
+                close: ohlc.close,
+            });
+        }
+    }
+    let mut out = File::create("../data/output.txt").expect("Failed to create file");
+    for ohls in ohlc_values {
+        let json_str = serde_json::to_string(&ohls).expect("Failed to serialize vector to JSON");
+        out.write_all(json_str.as_bytes()).expect("Failed to write to file");
+        out.write("\n".as_bytes()).expect("Failed to write to file");
+    }
 }
